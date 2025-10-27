ff183995d3142642b2ddbd80ae219342783d78df
Luis <luis.h.rios@unl.edu.ec>
2025-10-27 11:31:48 -0500

    Creación del menu de la practica

--- src/main/java/edu/unl/cc/execute/JLaboratoryPractice.java
+++ src/main/java/edu/unl/cc/execute/JLaboratoryPractice.java
@@ -4,6 +4,12 @@
 
 package edu.unl.cc.execute;
 
+import edu.unl.cc.Quadratic_Equation.QuadraticEquationExecute;
+import edu.unl.cc.account_bank.Bank_Execute;
+import edu.unl.cc.electronic_lock.ElectronicLockExecute;
+import edu.unl.cc.fraction.FractionExecute;
+import java.util.Scanner;
+
 /**
  *
  * @author luisr
@@ -11,6 +17,31 @@ package edu.unl.cc.execute;
 public class JLaboratoryPractice {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n=====Practica de Laboratorio=====\n");
+        System.out.println("Seleccione una opcion: ");
+        System.out.println("\n1.    Cerradura Electronica");
+        System.out.println("2.  Cuenta Bancaria");
+        System.out.println("3.  Ecuacion Cuadratica");
+        System.out.println("4.  Fraccion");
+        
+        int opc = scanner.nextInt();
+        
+        switch (opc) {
+            case 1:
+                ElectronicLockExecute.main(args);
+                break;
+                
+            case 2:
+                Bank_Execute.main(args);
+                
+            case 3:
+                QuadraticEquationExecute.main(args);
+                
+            case 4:
+                FractionExecute.main(args);
+            default:
+                throw new AssertionError();
+        }
     }
 }
